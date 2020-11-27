ae04fff9c06663e8e76877fc2eadbd2f6c4347d6
63070052 <73658351+63070052@users.noreply.github.com>
2020-11-27 12:37:58 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -14,7 +14,8 @@
 <body class="bg-light py-5">
 	<?php
 		$conn = mysqli_connect('testlabitf.mysql.database.azure.com', 'test@testlabitf', 'YHVexv86', 'itf');
-		$sql = 'DELETE FROM guestbook WHERE id = '.$_GET['id'].'';
+		
+		$sql = 'DELETE FROM guestbook WHERE id = "$id"';
 		echo $sql;
 	?>
 	<div class="container text-dark">
